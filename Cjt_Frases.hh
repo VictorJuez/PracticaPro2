d783f7576b4e513656197df450c6e252c9ade286
victor <victorjuez.alu@gmail.com>
2016-05-02 12:42:54 +0200

    updates

--- Cjt_Frases.hh
+++ Cjt_Frases.hh
@@ -1,51 +1,79 @@
+/** @file Cjt_Frases.hh
+    @brief Especificació de la clase Cjt_Frases
+*/
+
+/** @class Cjt_Frases
+    @brief Conté un conjunt de frases.
+
+    Todas las operaciones son de <b>coste constante</b>
+*/
 class Cjt_Frases{
 
 private:
 
 public:
 	// Constructores
-
+	/** @brief Creadora por defecte. 
+		S'executa automàticament al declarar un cjt_frases
+		\pre <em>Cert</em>
+		\post El resultat és un conjunt de frases buit.
+	*/ 
 	Cjt_Frases();
-	/* Pre: cert */
-	/* Post: el resultat és un conjunt de frases buit*/
 	
 	//Modificadores
 	
+	/** @brief Substitueix una paraula per una altre. 
+		\pre <em>La paraula1 ha d'apareixer com a mínim un cop en el paràmetre implícit.</em>
+		\post S'han substituït totes les paraules coincidents a paraula1 del paràmetre implícit per paraula2.
+	*/ 
 	void substituir_paraula(string paraula1, string paraula2);
-	/* Pre: la paraula1 ha d'apareixer com a mínim un cop en el paràmetre implícit*/
-	/* Post: s'han substituït totes les paraules coincidents a paraula1 del paràmetre implícit i s'han substituït per paraula2*/ 
 
 	//Destructora
 	
+	~Cjt_Frases();
 	//Consultores
-
+	
+	/** @brief Consultora del número de paraules. 
+		\pre <em>Cert</em>
+		\post El resultat es el número de paraules del paràmetre implícit.
+	*/
 	int numero_de_paraules() const;
-	/*Pre: cert*/
-	/*Post: el resultat es el numero de paraules del paràmetre implícit*/
-        
-        int numero_de_frases() const;
-	/*Pre: cert*/
-	/*Post: el resultat es el numero de frases del paràmetre implícit*/
-
+	
+	/** @brief Consultora del número de frases. 
+		\pre <em>Cert</em>
+		\post El resultat es el número de frases del paràmetre implícit
+	*/       
+	int numero_de_frases() const;
+	
+	/** @brief Consultora conté paraula. 
+		\pre <em>El vector de paraules no és buit</em>
+		\post El resultat indica si el paràmetre implícit conté o no la paraula
+	*/  
 	bool conte_paraula(string paraula) const;
-	/* Pre: cert */
-	/* Post: el resultat indica si el paràmetre implícit conté o no la paraula*/
-
+	
+	/** @brief Consultora paraules. 
+		\pre <em>el vector paraules no és buit</em>
+		\post el resultat ens indica si la frase conté la mateixa seqüència de paraules.
+	*/  
 	bool conte_paraules(vector<string> paraules) const;
-	/* Pre: el vector paraules no és buit*/
-	/* Post: el resultat ens indica si la frase conté la seqüència paraules en el mateix ordre*/
 	
 	//Lectura i escriptura
-	
-	void taula_frequencies() const;
-        /*Pre: cert*/
-        /*Post: s'ha escrit la taula de frequencies del paràmetre implícit al canal estàndar de sortida*/
-	
-	void llegir();
-	/* Pre: estan preparats al canal estandar d'entrada una seqüència de frases acabada en **** */
-	/*Post: el paràmetre implícit passa a tenir els atributs llegits pel canal estàndard d'entrada*/
 
+	/** @brief Imprimeix la taula de freqüènces. 
+		\pre <em>cert</em>
+		\post s'ha escrit la taula de frequencies del paràmetre implícit al canal estàndar de sortida
+	*/
+	void taula_frequencies() const;
+
+	/** @brief Llegeix un Cjt_Frases. 
+		\pre <em>Estan preparats al canal estandar d'entrada una seqüència de frases acabada en ****</em>
+		\post El paràmetre implícit passa a tenir els atributs llegits pel canal estàndard d'entrada.
+	*/
+	void llegir();
+
+	/** @brief Llegeix un Cjt_Frases. 
+		\pre <em>cert</em>
+		\post S'ha escrit la seqüència de frases del paràmetre implícit al canal estàndar de sortida.
+	*/	
 	void escriure() const;
-	/* Pre: cert*/
-	/* Post: s'ha escrit la seqüència de frases del paràmetre implícit al canal estàndar de sortida*/
 }
--- Frase.hh
+++ Frase.hh
@@ -1,49 +1,79 @@
+/** @file Frase.hh
+    @brief Especificación de la clase Frase
+*/
 
 
-Class Frase{
+/** @class Frase
+    @brief Conté un conjunt de paraules que formen la frase i aquestes poden estar separades per comes, punts i comes i dos punts. Acabada en '.', '?' o '!'.
 
-//Tipus de mòdul: dades
-//Descripció del tipus: Conté un conjunt de paraules que formen la frase i aquestes poden estar separades per comes, punts i comes i dos punts. Tota frase //està acabada en interrogant de tancar, admiració de tancar o en punt. Conté la variable num_paraules , la cual ens indica quantes paraules conté la
-//frase.
+    Todas las operaciones son de <b>coste constante</b>
+*/
+class Frase{
 
 private:
 
 public:
 	// Constructores
-
+	
+	/** @brief Creadora per defecte. 
+		S'executa automàticament al declarar una frase.
+		\pre <em>Cert</em>
+		\post El resultat és una frase buida.
+	*/ 
 	Frase();
-	/* Pre: cert */
-	/* Post: el resultat és una frase buida*/
 	
 	//Modificadores
 	
+	/** @brief Modificadora de frase.
+
+		\pre <em>La paraula1 ha d'apareixer com a mínim un cop en el paràmetre implícit</em>
+		\post S'han substituït totes les paraules coincidents a paraula1 del paràmetre implícit per paraula2
+	*/ 
 	void substituir_paraula(string paraula1, string paraula2);
-	/* Pre: la paraula1 ha d'apareixer com a mínim un cop en el paràmetre implícit*/
-	/* Post: s'han substituït totes les paraules coincidents a paraula1 del paràmetre implícit i s'han substituït per paraula2*/ 
 
 	//Destructora
+	/** @brief Destructora per defecte.
+		\pre <em>Cert</em>
+		\post El resultat és la frase eliminada.
+	*/ 	
+	~Frase();
 	
 	//Consultores
+	/** @brief Consultora de numero de paraules. 
+
+		\pre <em>Cert</em>
+		\post El resultat es el número de paraules del paràmetre implícit.
+	*/ 
 
 	int numero_de_paraules() const;
-	/*Pre: cert*/
-	/*Post: el resultat es el numero de paraules del paràmetre implícit*/
 
+	/** @brief Consultora conté paraula. 
+
+		\pre <em>Cert</em>
+		\post El resultat indica si el paràmetre implícit conté o no la paraula.
+	*/
 	bool conte_paraula(string paraula) const;
-	/* Pre: cert */
-	/* Post: el resultat indica si el paràmetre implícit conté o no la paraula*/
+	
+	/** @brief Consultora de numero de paraules. 
 
+		\pre <em>Cert</em>
+		\post El resultat indica si el paràmetre implícit conté o no la seqüència de paraules.
+	*/
 	bool conte_paraules(vector<string> paraules) const;
-	/* Pre: el vector paraules no és buit*/
-	/* Post: el resultat ens indica si la frase conté la seqüència paraules en el mateix ordre*/
 	
 	//Lectura i escriptura
 	
-	void llegir();
-	/* Pre: estan preparats al canal estandar d'entrada una seqüència de paraules i/o signes de puntuació no buit, acabada en punt, signe d'interrocació de tancament o signe d'exclamació de tancament*/
-	/*Post: el paràmetre implícit passa a tenir els atrivuts llegits pel canal estàndard d'entrada*/
+	/** @brief Lectura de paraules. 
 
+		\pre <em>Estan preparats al canal estandar d'entrada una seqüència de paraules i/o signes de puntuació no buit, acabada en punt, signe d'interrocació de tancament o signe d'exclamació de tancament</em>
+		\post El paràmetre implícit passa a tenir els atributs llegits pel canal estàndard d'entrada.
+	*/	
+	void llegir();
+	
+	/** @brief Lectura de paraules. 
+
+		\pre <em>Cert</em>
+		\post S'ha escrit la seqüència de paraules del paràmetre implícit al canal estàndar de sortida.
+	*/	
 	void escriure() const;
-	/* Pre: cert*/
-	/* Post: s'ha escrit la seqüència de paraules del paràmetre implícit al canal estàndar de sortida*/
 }
