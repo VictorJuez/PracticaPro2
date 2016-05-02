e174199356c7229c6039bd535d3872b9904eb34b
Enric <enric.bover@hotmail.es>
2016-05-02 12:37:58 +0200

    text

--- Text.hh
+++ Text.hh
@@ -1,51 +1,46 @@
-class Cjt_Frases{
+class Text{
 
 private:
 
 public:
 	// Constructores
 
-	Cjt_Frases();
+	Text();
 	/* Pre: cert */
-	/* Post: el resultat és un conjunt de frases buit*/
+	/* Post: el resultat és un Text sense autor, ni títol, ni contingut, ni cites*/
 	
 	//Modificadores
 	
 	void substituir_paraula(string paraula1, string paraula2);
-	/* Pre: la paraula1 ha d'apareixer com a mínim un cop en el paràmetre implícit*/
-	/* Post: s'han substituït totes les paraules coincidents a paraula1 del paràmetre implícit i s'han substituït per paraula2*/ 
-
+	/* Pre: la paraula1 ha d'apareixer com a mínim un cop en el contingut del paràmetre implícit*/
+	/* Post: s'han substituït totes les paraules coincidents a paraula1 del contingut del paràmetre implícit i s'han substituït per paraula2*/ 
+	
 	//Destructora
 	
+	~Text();
+	
 	//Consultores
-
-	int numero_de_paraules() const;
-	/*Pre: cert*/
-	/*Post: el resultat es el numero de paraules del paràmetre implícit*/
+	
+        string consultar_autor();
+        /*Pre: el text té autor*/
+        /*Post: el resultat és l'autor del text en qüestió*/
         
-        int numero_de_frases() const;
-	/*Pre: cert*/
-	/*Post: el resultat es el numero de frases del paràmetre implícit*/
-
-	bool conte_paraula(string paraula) const;
-	/* Pre: cert */
-	/* Post: el resultat indica si el paràmetre implícit conté o no la paraula*/
-
-	bool conte_paraules(vector<string> paraules) const;
-	/* Pre: el vector paraules no és buit*/
-	/* Post: el resultat ens indica si la frase conté la seqüència paraules en el mateix ordre*/
+        string consultar_titol();
+        /*Pre: el text té títol*/
+        /*Post: el resultat és el títol del text en qüestió*/
+        
+        Cjt_Frases consultar_contingut();
+        /*Pre: el text té contingut*/
+        /*Post: el resultat és el contingut del text en qüestió*/
 	
 	//Lectura i escriptura
 	
-	void taula_frequencies() const;
-        /*Pre: cert*/
-        /*Post: s'ha escrit la taula de frequencies del paràmetre implícit al canal estàndar de sortida*/
 	
 	void llegir();
-	/* Pre: estan preparats al canal estandar d'entrada una seqüència de frases acabada en **** */
+	/* Pre: estan preparats al canal estandar d'entrada un títol, un autor i un contingut */
 	/*Post: el paràmetre implícit passa a tenir els atributs llegits pel canal estàndard d'entrada*/
 
 	void escriure() const;
 	/* Pre: cert*/
-	/* Post: s'ha escrit la seqüència de frases del paràmetre implícit al canal estàndar de sortida*/
+	/* Post: s'ha escrit el títol, l'autor i el contingut pel canal estandar de sortida.*/
 }
