a5dea62a379731df33384e6ce899903cb4927ecd
Enric <enric.bover@hotmail.es>
2016-05-02 21:35:03 +0200

    ebwo

--- main.cc
+++ main.cc
@@ -83,8 +83,12 @@ int main(){
         }
         else if (op == "info"){
             is >> op;
-            if (op!= '?') cout << "Error" << endl;
-            else{
+            if (op == "cita"){
+                string referencia;
+                iss >> referencia;
+                cites.escriure_cita(referencia);
+            }
+            else if (op == '?'){
                 string autor = text_triat.consultar_autor();
                 string titol = text_triat.consultar_titol();
                 cout << autor << ' ' << titol << ' ';
@@ -95,10 +99,98 @@ int main(){
                 nparaules = frases.numero_de_paraules();
                 cout << autor << ' ' << titol << ' ' << nfrases << ' ' << nparaules << endl;
             }
+            else cout << "Error" << endl;
         }
         else if (op == "autor"){
             is >> op;
             if (op != '?') cout << "Error" << endl;
+            else cout << text_triat.consultar_autor() << endl;
         }
+        else if (op == "contingut"){
+            is >> op;
+            if (op != '?') cout << "Error" << endl;
+            else text_triat.consultar_contingut.escriure();
+        }
+        else if (op == "frases"){
+            int x, y;
+            iss >> x;
+            iss >> y;
+            iss >> op;
+            if (op != '?') cout << "Error" << endl;
+            else{
+                Cjt_frases frases = text_triat.consultar_contingut();
+                Cjt_frases aux = frases.frases_xy(x,y);
+                aux.escriure();
+            }
+            
+        }
+        else if (op == "nombre"){
+            iss >> op;
+            if (op == "frases"){
+                iss >> op;
+                if (op != '?') cout << "Error" << endl;
+                else{
+                    int nfrases = text_triat.consultar_contingut.numero_de_frases();
+                    cout << nfrases << endl;
+                }
+            }
+            else if (op == "paraules"){
+                iss >> op;
+                if (op != '?') cout << "Error" << endl;
+                else{
+                    int nparaules = text_triat.consultar_contingut.numero_de_paraules();
+                    cout << nparaules << endl;
+                }
+            }
+        }
+        else if (op == "taula"){
+            iss >> op;
+            if (op == "de"){
+                iss >> op;
+                if (op == "frequencies"){
+                    iss >> op;
+                    if (op == '?'){
+                        Cjt_frases frases = text_triat.consultar_contingut();
+                        frases.escriure_taula_frequencies();
+                    }
+                    else cout << "Error" < endl;
+                }
+                else cout << "Error" < endl;
+            }
+            else cout << "Error" < endl;
+        }
+        else if (op == "cites"){
+            iss >> op;
+            if (op == "autor"){
+                string autor;
+                iss >> autor;
+                cites.escriure_cites_autor(autor);
+            }
+            else if (op == '?'){
+                cites.escriure_cita_triat();
+            }
+            else cout << "Error" << endl;
+        }
+        else if (op == "totes"){
+            iss >> op;
+            if (op != 'cites') cout << "Error" << endl;
+            else{
+                iss >> op;
+                if (op == '?')cites.escriure_cites();
+                else cout << "Error" << endl;
+            }
+        }
+        else if (op == "eliminar"){
+            iss >> op;
+            if (op == "cita"){
+                string referencia;
+                iss >> referencia;
+                cites.eliminar_cita(referencia);
+            }
+            else cout << "Error" << endl;
+        }
+        else cout << "Error" << endl;
+        getline (cin,linia);
+        
     }
 }
